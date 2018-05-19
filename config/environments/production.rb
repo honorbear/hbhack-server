e544b075b6b55eadd72f2a514951a0f72558c23c
darren.chang <darren987469@gmail.com>
2018-05-19 13:41:55 +0800

    Setup db to posgtresql

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "hbhack_server_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "hbhack-server_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
